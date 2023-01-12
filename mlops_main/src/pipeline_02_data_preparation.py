e4b888b3a9397a26f6f60797214d81a0c0e32131
Abhijith Sai Thirunahari <>
2023-01-12 03:22:58 -0500

    implemented the logic for data preparation. Saved the files in temp location

--- mlops_main/src/pipeline_02_data_preparation.py
+++ mlops_main/src/pipeline_02_data_preparation.py
@@ -1,68 +1,78 @@
+import glob
 import logging
 import os
-
+import argparse
+from mlops_main import project_secrets
 from mlops_main.src.utils.common import read_yaml
+import pandas as pd
 
-STAGE = "State 04 feature engineering"
+STAGE = "State 02 Data Importing and Feature Engineering"
 
+log_path = project_secrets.log_path
+home_path = project_secrets.home_path
 
 logging.basicConfig(
-    filename=os.path.join("logs", 'running_logs.log'),
-    level=logging.INFO,
+    filename=os.path.join(home_path,log_path,'running_logs.log'),
+    level=logging.DEBUG,
     format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
     filemode="a"
     )
 
 
-def main(config_path, params_path):
+def main(config_path):
     # TO DO
-    # 1. Importing the data -> using YAML
-    # 2. Create a dataframe -> pandas dataframe
     # 3. Exploratory Data Analysis (Matplotlib and sklearn)
-    # 4. Prepare and stage the data (stage into a temp location)
+    # 4. Prepare and archive the data (stage into a temp location)
 
 
     ## read config files
-    config = read_yaml(config_path)
-    params = read_yaml(params_path)
+    config = read_yaml(os.path.join(home_path,config_path))
 
-    artifacts = config["artifacts"]
-    prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPARED_DATA"])
-    train_data_path = os.path.join(prepared_data_dir_path, artifacts["TRAIN_DATA"])
-    test_data_path = os.path.join(prepared_data_dir_path, artifacts["TEST_DATA"])
+    # 1. Importing the data -> using YAML & Creating the data frame
+    data_path = os.path.join(home_path,config['data_source']['batch_files'])
+    csv_files = glob.glob(data_path + "/*.csv")
+    logging.info(">>>>>>>>>>2.1 Importing the Data  <<<<<<<<<")
+    df_list = (pd.read_csv(file) for file in csv_files)
+    df = pd.concat(df_list,ignore_index=True)
+    logging.info(">>>>>>>>>> Importing the Data --> SUCCESS <<<<<<<<<")
 
-    featurized_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["FEATURIZED_DATA"])
-    create_directories([featurized_data_dir_path])
+    # 2. Feature Engineering
+    columns = df.columns
+    logging.info(">>>>>>>>>> 2.2 Feature Engineering <<<<<<<<<")
+    logging.info(">>>>>>>>>> Displaying first 5 rows of the dataframe <<<<<<<<<")
+    logging.info(df.count())
+    logging.info(">>>>>>>>Columns in the data set are <<<<<<<< \n ")
+    logging.info(columns)
 
-    featurized_train_data_path = os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_TRAIN_DATA"])
-    featurized_test_data_path = os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_TEST_DATA"])
+    # 3. Exploratory Data Analysis
+    # TO DO
 
-    df_train = get_df(train_data_path)
+    # 4. Archiving the data
+    archive_path = config['data_preparation']['archive']
+    archive_path = os.path.join(home_path,archive_path)
 
-    train_words = np.array(df_train.text.str.lower().values.astype("U")) ## << U1000
+    if (os.path.exists(archive_path)):
+        logging.info("Archive path Already exists. Saving the file to the location ")
+        df.to_csv(os.path.join(archive_path,'data.csv'))
+    else:
+        logging.info("Archive path does not exist. creating the file location ")
+        os.mkdir(archive_path)
+        logging.info("Created the file location and Saving the file to the location")
+        df.to_csv(os.path.join(archive_path,'data.csv'))
 
-    max_features = params["featurize"]["max_features"]
-    ngrams = params["featurize"]["ngrams"]
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="mlops/mlops_main/config/config.yaml")
+    parsed_args = args.parse_args()
 
-    bag_of_words = CountVectorizer(
-        stop_words="english", max_features=max_features, ngram_range=(1, ngrams)
-    )
-
-    bag_of_words.fit(train_words)
-    train_words_binary_matrix = bag_of_words.transform(train_words)
-
-    tfidf = TfidfTransformer(smooth_idf=False)
-    tfidf.fit(train_words_binary_matrix)
-    train_words_tfidf_matrix = tfidf.transform(train_words_binary_matrix)
-    save_matrix(df_train, train_words_tfidf_matrix, featurized_train_data_path)
-
-    df_test = get_df(test_data_path)
-    test_words = np.array(df_test.text.str.lower().values.astype("U")) ## << U1000
-
-    test_words_binary_matrix = bag_of_words.transform(test_words)
-
-    test_words_tfidf_matrix = tfidf.transform(test_words_binary_matrix)
-    save_matrix(df_test, test_words_tfidf_matrix, featurized_test_data_path)
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
 
 
 
